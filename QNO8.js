7bdfe114e8df45ab09b5b7d9d24ae30cbaf1d132
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-03-14 00:13:12 +0500

    Add files via upload

---
+++ QNO8.js
@@ -0,0 +1,13 @@
+"use strict";
+// 8. You should create four lines that look like this:
+// console.log(5 + 3)
+// Your output should simply be four lines with the number 8 appearing once on each line.
+Object.defineProperty(exports, "__esModule", { value: true });
+// NO1)ADDITION +
+console.log(5 + 3);
+// 2)SUBTRACTION -
+console.log(18 - 10);
+// 3)MULTIPLICATION
+console.log(2 * 4);
+// 4)DIVISION
+console.log(16 / 2);
---
+++ QNO8.ts
@@ -0,0 +1,17 @@
+// 8. You should create four lines that look like this:
+// console.log(5 + 3)
+// Your output should simply be four lines with the number 8 appearing once on each line.
+
+
+// NO1)ADDITION +
+console.log(5 + 3);
+
+// 2)SUBTRACTION -
+console.log(18 - 10);
+
+// 3)MULTIPLICATION
+console.log(2 * 4);
+
+// 4)DIVISION
+console.log(16 / 2);
+
